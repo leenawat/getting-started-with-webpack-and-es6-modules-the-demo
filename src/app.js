fdb2f8e97dabc29112117eb6d2f6389a55562e15
Leenawat <leenawat.pa@gmail.com>
2019-10-08 19:23:45 +0700

    test click

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-import {sayHello} from './modules/greeting';
-import {sum, product} from './modules/math-functions';
+import { sayHello } from './modules/greeting';
+import { sum, product } from './modules/math-functions';
 
 const resultGreeting = document.getElementById('resultGreeting');
 const resultSum = document.getElementById('resultSum');
@@ -11,3 +11,17 @@ const b = 7;
 resultGreeting.textContent = sayHello('Nice to see you! 🙂');
 resultSum.textContent = `The sum of ${a} and ${b} is ${sum(a, b)}. ✨`;
 resultProduct.textContent = `The product of ${a} and ${b} is ${product(a, b)}. 🚀`;
+
+document.addEventListener('DOMContentLoaded', () => {
+  // console.log("xxx");
+  // var xbtn = document.getElementById("xbtn");
+  // xbtn.addEventListener('click', () => {
+  //   console.log("xbtn click");
+  //   console.log(sum(a, b));
+  // });
+  $("#xbtn").on("click", () => {
+    console.log("xbtn click");
+    console.log(sum(a, b));
+    console.log(product(a, b));
+  })
+})
